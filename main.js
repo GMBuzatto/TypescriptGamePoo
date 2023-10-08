9302c6349565ea7bf9b01f33c389bc612f2e9f1f
GMBuzatto <gabrielmartinbuzatto1999@gmail.com>
2023-10-08 16:39:26 -0300

    feat: adiciona menu inicial

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("Teste");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const prompt_sync_1 = __importDefault(require("prompt-sync"));
+let teclado = (0, prompt_sync_1.default)();
+let menuOption = 0;
+while (menuOption != 3) {
+    console.log("@---TypescriptGamePoo---@");
+    console.log("| 1 - Jogar             |");
+    console.log("| 2 - Criar Personagem  |");
+    console.log("| 3 - Sair              |");
+    console.log("@-----------------------@");
+    menuOption = +teclado("Escolha uma opção: ");
+}
--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
-console.log("Teste")
+import prompt from "prompt-sync"
+
+
+let teclado = prompt()
+let menuOption = 0
+
+while (menuOption != 3) {
+    console.log("@---TypescriptGamePoo---@")
+    console.log("| 1 - Jogar             |")
+    console.log("| 2 - Criar Personagem  |")
+    console.log("| 3 - Sair              |")
+    console.log("@-----------------------@")
+    menuOption = + teclado("Escolha uma opção: ")
+
+}
