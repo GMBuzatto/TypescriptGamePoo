73ad59b8cac1ddc1f346547231411c4c34544cbb
GMBuzatto <gabrielmartinbuzatto1999@gmail.com>
2023-10-08 17:41:22 -0300

    feat: adiciona opção 3 criar persoangem

--- main.js
+++ main.js
@@ -4,13 +4,38 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const prompt_sync_1 = __importDefault(require("prompt-sync"));
+const Humano_1 = require("./Personagem/Racas/Humano");
+const Orc_1 = require("./Personagem/Racas/Orc");
 let teclado = (0, prompt_sync_1.default)();
 let menuOption = 0;
-while (menuOption != 3) {
+while (menuOption != 4) {
     console.log("@---TypescriptGamePoo---@");
     console.log("| 1 - Jogar             |");
-    console.log("| 2 - Criar Personagem  |");
-    console.log("| 3 - Sair              |");
+    console.log("| 2 - Status            |");
+    console.log("| 3 - Criar Personagem  |");
+    console.log("| 4 - Sair              |");
     console.log("@-----------------------@");
     menuOption = +teclado("Escolha uma opção: ");
+    switch (menuOption) {
+        case 3:
+            console.clear();
+            console.log("@---TypescriptGamePoo---@");
+            console.log("| Escolha uma raça:     |");
+            console.log("| 1 - Humano            |");
+            console.log("| 2 - Orc               |");
+            console.log("| 3 - Voltar            |");
+            console.log("@-----------------------@");
+            let racaOption = +teclado("\n Opção: ");
+            if (racaOption == 1) {
+                let nome = teclado("Escolha seu nome: ");
+                let sobreNome = teclado("Escolha seu sobre nome: ");
+                let personagem = new Humano_1.Humano(nome, sobreNome);
+            }
+            else {
+                let nome = teclado("Escolha seu nome: ");
+                let pet = teclado("Escolha o nome do seu pet: ");
+                let personagem = new Orc_1.Orc(nome, pet);
+            }
+            break;
+    }
 }
--- main.ts
+++ main.ts
@@ -1,15 +1,40 @@
 import prompt from "prompt-sync"
+import { Personagem } from "./Personagem/Personagem"
+import { Humano } from "./Personagem/Racas/Humano"
+import { Orc } from "./Personagem/Racas/Orc"
 
 
 let teclado = prompt()
 let menuOption = 0
 
-while (menuOption != 3) {
+while (menuOption != 4) {
     console.log("@---TypescriptGamePoo---@")
     console.log("| 1 - Jogar             |")
-    console.log("| 2 - Criar Personagem  |")
-    console.log("| 3 - Sair              |")
+    console.log("| 2 - Status            |")
+    console.log("| 3 - Criar Personagem  |")
+    console.log("| 4 - Sair              |")
     console.log("@-----------------------@")
     menuOption = + teclado("Escolha uma opção: ")
 
+    switch (menuOption) {
+        case 3:
+            console.clear()
+            console.log("@---TypescriptGamePoo---@")
+            console.log("| Escolha uma raça:     |")
+            console.log("| 1 - Humano            |")
+            console.log("| 2 - Orc               |")
+            console.log("| 3 - Voltar            |")
+            console.log("@-----------------------@")
+            let racaOption = + teclado("\n Opção: ")
+            if (racaOption == 1) {
+                let nome: string = teclado("Escolha seu nome: ")
+                let sobreNome: string = teclado("Escolha seu sobre nome: ")
+                let personagem: Personagem = new Humano(nome, sobreNome)
+            } else {
+                let nome: string = teclado("Escolha seu nome: ")
+                let pet: string = teclado("Escolha o nome do seu pet: ")
+                let personagem: Personagem = new Orc(nome, pet)
+            }
+            break;
+    }
 }
